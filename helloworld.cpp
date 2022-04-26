d113417a9357c863e7892bf76458dfb204c6d3c9
Georgesatchy <76107533+Georgesatchy@users.noreply.github.com>
2022-04-26 16:23:35 +0100

    hello world

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
-    return 0;
+    const int x = 5;
+    if (x > 5) {
+        return 1;
+    } else if (x == 5) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
