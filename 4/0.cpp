0a25de0e815934678042e38ea346c00aea52d240
Georgesatchy <76107533+Georgesatchy@users.noreply.github.com>
2022-05-24 15:44:16 +0100

    Lesson 4

--- 4/0.cpp
+++ 4/0.cpp
@@ -37,20 +37,55 @@ Employee search(string name, Employee* employees) {
     return (Employee &&) "null";
 }
 
-Employee newEmployee(string name, int employeeNumber, int salary) {
+Employee newEmployee(int employeeNumber, string name, Date dob, double salary, string department) {
     Employee thisEmployee;
-    thisEmployee.name = name;
     thisEmployee.employeeNumber = employeeNumber;
+    thisEmployee.name = name;
+    thisEmployee.dob = dob;
     thisEmployee.salary = salary;
+    thisEmployee.department = department;
     return thisEmployee;
 }
 
 int main() {
-    Employee employees[] = {newEmployee("george", 0, 9832974)};
-    Date DOB;
-    DOB.day = 2;
-    DOB.month = 8;
-    DOB.year = 2005;
-    cout << DOB << endl;
-    cout << search("george", employees);
+    Employee employees[] = {};
+    while (1) {
+        int choice;
+        string name;
+        Employee thisEmployee;
+        int employeeNumber;
+        Date DOB;
+        double salary;
+        string department;
+        cout << "Choose a function:" << endl << "1. Search for an employee by name" << endl
+        << "2. Add a new employee" << endl
+        << "3. Remove an employee " << endl;
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cout << "Enter employee name:" << endl;
+                cin >> name;
+                thisEmployee = search(name, employees);
+                break;
+            case 2:
+                cout << "Enter employee name:" << endl;
+                cin >> name;
+                cout << "Enter employee DOB (DD MM YYYY):" << endl;
+                cin >> DOB.day >> DOB.month >> DOB.year;
+                cout << "Enter employee salary:" << endl;
+                cin >> salary;
+                cout << "Enter employee department:" << endl;
+                cin >> department;
+                employees[sizeof(*employees)/sizeof(employees[0])] = newEmployee(
+                        sizeof(*employees)/sizeof(employees[0]),
+                        name, DOB, salary, department);
+                break;
+            case 3:
+                cout << "WORK IN PROGRESS" << endl
+                break;
+            default:
+                cout << "Invalid Choice!" << endl;
+                break;
+        }
+    }
 }
